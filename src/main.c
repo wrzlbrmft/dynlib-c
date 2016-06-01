df7d53d72aed31871a5a8515aac9a64f234f8601
matthias <matthias@calvin>
2016-06-01 22:09:00 +0200

    - lazy loading - comments and ptr formatting

--- src/main.c
+++ src/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
+	// expect first arg to be lib file to load
 	if (1 == argc) {
 		fprintf(stderr, "error: no lib file\n");
 		return 1;
 	}
-	char* lib = argv[1];
+	char *lib = argv[1];
 
+	// load lib file or exit on error
 	printf("loading lib %s\n", lib);
-	void* lib_ptr = dlopen(lib, RTLD_NOW);
+	void *lib_ptr = dlopen(lib, RTLD_LAZY);
 	if (!lib_ptr) {
 		fprintf(stderr, "error loading lib %s (%s)\n", lib, dlerror());
 		return 1;
