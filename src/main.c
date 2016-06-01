83c7252bd918fd126e322a924ed53f9776d30be3
matthias <matthias@calvin>
2016-06-01 22:04:00 +0200

    now loading lib

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <dlfcn.h>
 
-int main() {
-	printf("Hello World!\n");
+int main(int argc, char* argv[]) {
+	if (1 == argc) {
+		fprintf(stderr, "error: no lib file\n");
+		return 1;
+	}
+	char* lib = argv[1];
+
+	printf("loading lib %s\n", lib);
+	void* lib_ptr = dlopen(lib, RTLD_NOW);
+	if (!lib_ptr) {
+		fprintf(stderr, "error loading lib %s (%s)\n", lib, dlerror());
+		return 1;
+	}
 
 	return 0;
 }
